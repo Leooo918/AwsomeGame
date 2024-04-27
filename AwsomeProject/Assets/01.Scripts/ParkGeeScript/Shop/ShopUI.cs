4f63892277e674a0defb10ba5d3043c2556c9af1
Qmais <ckscks1011@naver.com>
2024-04-27 10:52:11 +0900

    shop

--- AwsomeProject/Assets/01.Scripts/ParkGeeScript/Shop/ShopUI.cs
+++ AwsomeProject/Assets/01.Scripts/ParkGeeScript/Shop/ShopUI.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
-public class ShopUI : MonoBehaviour
+public class ShopUI : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private Image selectedItemOn;
-    [SerializeField] private Image[] items;
-    //[SerializeField] private GameObject[] itemsBtn;
+    [SerializeField] private Image _selectedItemOn;
+    [SerializeField] private TextMeshProUGUI _priceTxt;
+    [SerializeField] private TextMeshProUGUI _descriptTxt;
+    [SerializeField, TextArea] private string _menual;
+    [SerializeField] private int _price;
 
-    public void ItemSelect()
+    public void OnPointerClick(PointerEventData eventData)
     {
-        for (int i = 0; i < items.Length; i++)
+        // 클릭한 객체가 이미지인지 확인해주기
+        if (eventData.pointerCurrentRaycast.gameObject.GetComponent<Image>() != null)
         {
-            selectedItemOn.sprite = items[i].sprite;
+            // 클릭한 이미지를 변수로저장
+            Image clickedImage = eventData.pointerCurrentRaycast.gameObject.GetComponent<Image>();
+
+            _selectedItemOn.sprite = clickedImage.sprite;
+            _descriptTxt.text = _menual;
+
+            _priceTxt.text = $"{_price}원";
         }
     }
 }
