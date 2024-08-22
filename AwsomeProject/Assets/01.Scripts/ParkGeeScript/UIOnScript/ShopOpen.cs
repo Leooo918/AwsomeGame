c366f9db51cdbde07df6f6b196796b7c2f62a46d
Qmais <ckscks1011@naver.com>
2024-08-22 17:12:03 +0900

    shop fix

--- AwsomeProject/Assets/01.Scripts/ParkGeeScript/UIOnScript/ShopOpen.cs
+++ AwsomeProject/Assets/01.Scripts/ParkGeeScript/UIOnScript/ShopOpen.cs
@@ -5,26 +5,58 @@ using UnityEngine;
 public class ShopOpen : MonoBehaviour
 {
     [SerializeField] private GameObject _interact;
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private float detectionRadius = 5f;
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private bool isPlayerInRange = false;
+
+    private void Update()
     {
-        if (collision.gameObject.CompareTag("Player"))
+        CheckPlayerDistance();
+    }
+
+    private void CheckPlayerDistance()
+    {
+        float distance = Vector2.Distance(transform.position, playerTransform.position);
+
+        if (distance <= detectionRadius)
         {
-            //UIManager.Instance.GuideOn();
-            _interact.SetActive(true);
+            if (!isPlayerInRange)
+            {
+                isPlayerInRange = true;
+                OnPlayerEnter();
+            }
+
             if (Input.GetKeyDown(KeyCode.F))
             {
                 UIManager.Instance.Open(UIType.Shop);
             }
         }
+        else
+        {
+            if (isPlayerInRange)
+            {
+                isPlayerInRange = false;
+                OnPlayerExit();
+            }
+        }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnPlayerEnter()
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            _interact.SetActive(false);
-            //UIManager.Instance.GuideOff();
-        }
+        _interact.SetActive(true);
+        //UIManager.Instance.GuideOn();
+    }
+
+    private void OnPlayerExit()
+    {
+        _interact.SetActive(false);
+        //UIManager.Instance.GuideOff();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawSphere(transform.position, detectionRadius);
     }
 }
