ff971f4a8b63aace783040a5a94c50945a632b0d
dongdong5899 <dong012673@gmail.com>
2024-09-26 18:02:05 +0900

    .

--- AwsomeProject/Assets/01.Scripts/JinSoonScript/UI/Option.cs
+++ AwsomeProject/Assets/01.Scripts/JinSoonScript/UI/Option.cs
@@ -11,13 +11,6 @@ using UnityEngine.UI;
 
 public class Option : MonoBehaviour, IManageableUI
 {
-    private string _path;
-    [SerializeField] private AudioMixer _mixer;
-
-    [SerializeField] private Slider _masterVolumeSlider;
-    [SerializeField] private Slider _bgmVolumeSlider;
-    [SerializeField] private Slider _sfxVolumeSlider;
-
     [Space(10)]
     [SerializeField] private RectTransform _optionRect;
     [SerializeField] private float _easeingDelay = 0.5f;
@@ -25,45 +18,8 @@ public class Option : MonoBehaviour, IManageableUI
     [SerializeField] private Vector2 _closeOffset;
     private Tween tween;
 
-    private float _master;
-    private float _bgm;
-    private float _sfx;
-
     public bool isOpened = false;
 
-    private void Awake()
-    {
-        _path = Path.Combine(Application.dataPath, "Option.txt");
-
-        _masterVolumeSlider.onValueChanged.AddListener(OnMasterVolumeChange);
-        _bgmVolumeSlider.onValueChanged.AddListener(OnBGMVolumeChange);
-        _sfxVolumeSlider.onValueChanged.AddListener(OnSFXVolumeChange);
-    }
-    public void OnMasterVolumeChange(float value)
-    {
-        _master = value;
-        _mixer.SetFloat("Master", value);
-    }
-    public void OnBGMVolumeChange(float value)
-    {
-        _bgm = value;
-        _mixer.SetFloat("BGM", value);
-    }
-    public void OnSFXVolumeChange(float value)
-    {
-        _sfx = value;
-        _mixer.SetFloat("SFX", value);
-    }
-
-    public void GoToTitle()
-    {
-        GameManager.Instance.GoToTitle();
-    }
-
-    public void RestartGame()
-    {
-        GameManager.Instance.Restart();
-    }
     public void Open()
     {
         if (tween != null && tween.active)
@@ -81,21 +37,6 @@ public class Option : MonoBehaviour, IManageableUI
 
         tween = _optionRect.DOAnchorPos(_closeOffset, _easeingDelay);
         isOpened = false;
-    }
-
-    public void Save()
-    {
-        string str = $"{_master},{_bgm},{_sfx}";
-        File.WriteAllText(_path, str);
-    }
-
-    public void Load()
-    {
-        string str = File.ReadAllText(_path);
-        string[] s = str.Split(',');
-        OnMasterVolumeChange(float.Parse(s[0]));
-        OnBGMVolumeChange(float.Parse(s[1]));
-        OnSFXVolumeChange(float.Parse(s[2]));
     }
 
     public void Init()
